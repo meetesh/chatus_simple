8d1e35962fd8b2ecd3c30df2dfdd67f1263b5637
MEETESH <kmeetesh@gmail.com>
2016-03-19 03:00:05 +0530

    Added ajax for getallusers

---
+++ ajax/GetAllUsers.php
@@ -0,0 +1,32 @@
+<?php
+
+require "common.php";
+require __DIR__."/../bl/UserManager.php";
+$userManager = new UserManager();
+try
+{
+$users = $userManager->getAllUsers();
+foreach($users as $user)
+{
+	foreach($user as $key=>$value)
+	{
+		$user->$key = htmlentities($value, ENT_QUOTES, 'UTF-8');
+	}
+}
+
+$res = array('success' => true,'users'=> $users );
+print json_encode($res);
+
+
+}catch(BLException $e)
+{
+	print "{";
+	print "\"success\":false,";
+	print "\"message\":\"".$e->getMessage()."\"";
+	print "}";
+}
+
+
+
+
+?>
